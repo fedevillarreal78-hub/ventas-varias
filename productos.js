2510a410be046aadd881027099ffe0e58f68cc65
fedevillarreal78-hub <fedevillarreal78@gmail.com>
2026-07-12 16:31:49 -0600

    Actualización de productos desde el panel

--- productos.js
+++ productos.js
@@ -1,4 +1,4 @@
-/* Archivo generado por el Panel de gestión (admin.html) — 12/7/2026, 4:31:14 p. m..
+/* Archivo generado por el Panel de gestión (admin.html) — 12/7/2026, 4:31:48 p. m..
    Puedes editarlo a mano o seguir usando el panel. */
 
 const CONFIG = {
